7abd6893490f8c97ac4272cf286bcf34cf3d6e08
Vlad <vlad.miryaha@gmail.com>
2018-04-16 22:51:46 +0300

    fixit

--- Vector3.cpp
+++ Vector3.cpp
@@ -13,13 +13,16 @@ double Vector3::len() {
 }
 
 bool Vector3::isPerpendicular(const Vector3 &other) const {
+    // опасно сравнивать с нулем вещественные числа. лучше ввести константу epsilon, и сравнивать с ней, а не с нулем
     return !(Vector3(x, y, z) * other);
 }
 
 bool Vector3::isCollinear(const Vector3 &other) const {
+    // код упадет, если other = (1, 0, 0) например
     return (x / other.x == y / other.y) && (y / other.y == z / other.z);
 }
 
+// лучше бы сделали методы x(), y(), z() , чтобы достучаться до компонент вектора, либо просто оставили их публичными
 void Vector3::getCoordinates(std::istream &in) {
     in >> x >> y >> z;
 }
@@ -38,6 +41,7 @@ std::ofstream &Vector3::fdump(std::ofstream &out) {
 }
 
 double Vector3::mixedProduct(const Vector3 &v1, Vector3 &v2) {
+    // вместо Vector3(x, y, z) можно было бы написать *this
     return Vector3(x, y, z) * v1.crossProduct(v2);
 }
 
@@ -46,6 +50,7 @@ Vector3 Vector3::crossProduct(const Vector3 &v) const {
 }
 
 bool Vector3::operator==(const Vector3 &other) {
+    // опасно сравнивать явно между собой вещественные числа, т.к. можно наткнуться на ошибки округления
     return ((x == other.x) && (y == other.y) && (z == other.z));
 }
 
@@ -67,6 +72,7 @@ Vector3 Vector3::operator*(double alpha) {
 }
 
 Vector3 operator*(double alpha, Vector3 v) {
+    // return v * alpha;
     return Vector3(v.x * alpha, v.y * alpha, v.z * alpha);
 }
 
@@ -89,3 +95,5 @@ std::ostream &operator<<(std::ostream &out, Vector3 v) {
     return v.dump(out);
 
 }
+
+// так-то можно было попробовать реализовать поворот вектора
